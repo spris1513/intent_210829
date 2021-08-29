de7326ef292236064c365f6dd7c96dc67205b8e2
이진호 <spris1513@gmail.com>
2021-08-29 14:51:59 +0900

    Intent 활용 > 메인화면에서 다른화면으로 이동

--- app/src/main/java/com/neppplus/intent_210829/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_210829/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.neppplus.intent_210829
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToOtherBtn.setOnClickListener {
+
+//              다른 화면으로 이동하기가 눌리면?
+
+//            어디서 → 어느 화면으로 이동할건지 정보(Intent) 명시 → 변수에 담아서 저장
+            val myIntent = Intent( this, OtherActivity::class.java )
+//              실제로 이동 시키자.
+            startActivity( myIntent)
+
+
+        }
     }
 }
