2ad9d3d8b2cd36ff02b39a0c1112c13d7608aacc
이진호 <spris1513@gmail.com>
2021-08-29 15:16:03 +0900

    입력한 내용 담아두기 + Intent 단순 화면 이동만

--- app/src/main/java/com/neppplus/intent_210829/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_210829/MainActivity.kt
@@ -10,6 +10,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        sendMessageBtn.setOnClickListener {
+
+//            메세지 보내기가 눌리면?
+
+//            입력한 내용이 어떻게 되는지 변수에 저장.
+            val inputContent = messageEdt.text.toString()
+
+//            메세지 조회 화면으로 (입력한 내용을 들고) 이동.
+            val myIntent3 = Intent(this,ViewMessageActivity::class.java)
+            startActivity(myIntent3)
+
+        }
+
         moveToOtherBtn.setOnClickListener {
 
 //              다른 화면으로 이동하기가 눌리면?
