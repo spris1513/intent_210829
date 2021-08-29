066ab71de21779343c371325a8700ae03db967b3
이진호 <spris1513@gmail.com>
2021-08-29 16:12:53 +0900

    startActivityForResult 사용해서 다른액티비티로 넘어가는거까지

--- app/src/main/java/com/neppplus/intent_210829/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_210829/MainActivity.kt
@@ -6,9 +6,27 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_CODE_FOR_NICKNAME = 1004
+    val REQ_FOR_PHONE_NUM = 1005
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editNicknameBtn.setOnClickListener {
+
+//            닉네임 변경 화면으로 이동.
+//            단순이동X, 새 닉네임을 받아오기 위한 왕복이동.
+
+//            Intent 변수 만드는 과정 동일.
+            val myIntent = Intent(this,EditNicknameActivity::class.java)
+
+//            데이터(닉네임)를 받아오기 위한 왕복 이동 명시.
+            startActivityForResult(myIntent, REQUEST_CODE_FOR_NICKNAME)
+
+
+        }
 
         sendMessageBtn.setOnClickListener {
 
